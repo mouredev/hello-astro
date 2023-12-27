4c6f57371e64abfb6dee37a2fc330bef70b050b9
Brais Moure <mouredev@gmail.com>
2023-12-27 09:59:37 +0100

    Commit inicial

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from 'astro/config';
+
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
+export default defineConfig({
+  output: "server",
+  adapter: vercel()
+});
